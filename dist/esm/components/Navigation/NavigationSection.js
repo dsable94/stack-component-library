d8cfc50b52f91b89851bba14b0e8ee64a8b4b7bd
itadmin <itadmin@nishantm.successivetech.com>
2021-07-25 15:15:12 +0530

    update build

--- dist/esm/components/Navigation/NavigationSection.js
+++ dist/esm/components/Navigation/NavigationSection.js
@@ -17,7 +17,7 @@ var _Avatar = _interopRequireDefault(require("../Avatar/Avatar"));
 
 var _Navigation = require("./Navigation.styled");
 
-var _excluded = ["title", "hasSeparator", "isWidth", "items", "active", "as", "className"];
+var _excluded = ["title", "hasSeparator", "isWidth", "items", "active", "as", "handleToggle", "setOpen", "open", "className"];
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -26,18 +26,6 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
@@ -50,17 +38,11 @@ var NavigationSection = function NavigationSection(props) {
       items = props.items,
       active = props.active,
       as = props.as,
+      handleToggle = props.handleToggle,
+      setOpen = props.setOpen,
+      open = props.open,
       customClassName = props.className,
       rest = _objectWithoutProperties(props, _excluded);
-
-  var _useState = (0, _react.useState)(true),
-      _useState2 = _slicedToArray(_useState, 2),
-      open = _useState2[0],
-      setOpen = _useState2[1];
-
-  var handleToggle = function handleToggle() {
-    setOpen(!open);
-  };
 
   return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, isWidth && /*#__PURE__*/_react["default"].createElement(_Navigation.StyledToggleButton, {
     onClick: handleToggle,
---
+++ storybook-static/precache-manifest.35276a5e1168468a05f768809fd17a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372d4f2c2a27b5bc0cd3c59f315f9ac7",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a531d20a2aae6dbc7e88.bundle.js"
+  },
+  {
+    "url": "runtime~main.a531d20a2aae6dbc7e88.bundle.js"
+  },
+  {
+    "revision": "404921adaebfaef21c53",
+    "url": "static/css/main.f4e0026c.chunk.css"
+  },
+  {
+    "revision": "649620af1833f042d2f0",
+    "url": "static/css/vendors~main.9233ae65.chunk.css"
+  },
+  {
+    "url": "vendors~main.a531d20a2aae6dbc7e88.bundle.js"
+  },
+  {
+    "url": "vendors~main.a531d20a2aae6dbc7e88.bundle.js.LICENSE.txt"
+  }
+]);
