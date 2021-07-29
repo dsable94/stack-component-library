fa21b023384f4d77a569911c256575795b6f8846
itadmin <itadmin@nishantm.successivetech.com>
2021-07-29 17:41:16 +0530

    create build

---
+++ storybook-static/precache-manifest.1fbb5c50d1adc3adfbb0cd228934fd74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ab401e6cca1da9f34296112b203399",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e51db243735154e11a50.bundle.js"
+  },
+  {
+    "url": "runtime~main.e51db243735154e11a50.bundle.js"
+  },
+  {
+    "revision": "03387fa7d79ac29c134b",
+    "url": "static/css/main.f4e0026c.chunk.css"
+  },
+  {
+    "revision": "649620af1833f042d2f0",
+    "url": "static/css/vendors~main.9233ae65.chunk.css"
+  },
+  {
+    "url": "vendors~main.e51db243735154e11a50.bundle.js"
+  },
+  {
+    "url": "vendors~main.e51db243735154e11a50.bundle.js.LICENSE.txt"
+  }
+]);
